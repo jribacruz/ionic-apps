b8eb76ee291cb0b391b0ef69a6b6ecc98838205d
jribacruz <jribacruz@gmail.com>
2017-04-17 21:20:57 -0300

    loadind de busca de dispositivos

--- bluetooth/src/pages/home/home.ts
+++ bluetooth/src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ToastController, AlertController, LoadingController } from 'ionic-angular';
 import { BluetoothSerial } from "@ionic-native/bluetooth-serial";
+import _ from "lodash";
 
 @Component({
   selector: 'page-home',
@@ -8,7 +9,11 @@ import { BluetoothSerial } from "@ionic-native/bluetooth-serial";
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController, private bluetoothSerial: BluetoothSerial) {
+  constructor(public navCtrl: NavController,
+    private bluetoothSerial: BluetoothSerial,
+    private toastCtrl: ToastController,
+    private alertCtrl: AlertController,
+    private loadingCtrl: LoadingController) {
 
   }
 
@@ -22,6 +27,7 @@ export class HomePage {
       // Lista os dispositivos ja pareados.
       this.bluetoothSerial.list().then(devices => {
         console.log(`br.bluetooth: ${JSON.stringify(devices)}`);
+
       });
     }).catch(res => {
       console.log('Erro ao conectar bluetooth. Ativado ?');
@@ -36,11 +42,38 @@ export class HomePage {
     this.bluetoothSerial.isEnabled().then(res => {
       console.log('Bluetooth ativado.');
       // Lista os dispositivos ja pareados.
-      this.bluetoothSerial.discoverUnpaired().then(devices => {
-        console.log(`br.bluetooth: ${JSON.stringify(devices)}`);
-      });
+      this.showBluetoothDevices();
     }).catch(res => {
-      console.log('Erro ao conectar bluetooth. Ativado ?');
+      //console.log('Erro ao conectar bluetooth. Ativado ?');
+      let toast = this.toastCtrl.create({
+        message: 'Bluetooth não ativado.',
+        showCloseButton: true
+      });
+      toast.present();
+    });
+  }
+
+  /**
+   * Exibe um alert com os dispositivos bluetooth disponíveis.
+   */
+  private showBluetoothDevices() {
+    let loading = this.loadingCtrl.create({
+      content: 'Buscando dispositivos...'
+    });
+    loading.present();
+    this.bluetoothSerial.discoverUnpaired().then(devices => {
+      loading.dismiss();
+      let alertBDevices = this.alertCtrl.create();
+      alertBDevices.setTitle('Dispositivos Bluetooth');
+      devices.forEach(device => {
+        alertBDevices.addInput({
+          type: 'radio',
+          label: `${device.name} ${device.address}`,
+          value: device.address
+        });
+        alertBDevices.addButton('Cancelar');
+        alertBDevices.present();
+      });
     });
   }
 
